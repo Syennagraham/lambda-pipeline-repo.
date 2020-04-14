35cdc71a0bee41d5077760f50d2f0287b4552f22
syenna <syenna.graham@gmail.com>
2020-04-14 17:41:12 -0400

    add hello

---
+++ hello.js
@@ -0,0 +1,10 @@
+var time = require('time');
+exports.handler = (event, context, callback) => {
+    var currentTime = new time.Date();
+    currentTime.setTimezone("America/Los_Angeles");
+    callback(null, {
+        statusCode: '200',
+        body: 'The good time in florida is: ' + currentTime.toString(),
+    });
+};
+
